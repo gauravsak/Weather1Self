9ded0dd36257859c1f78cdd05061c68d781fac86
Gaurav Sakhardande <sakhardande.gaurav@gmail.com>
2015-03-15 16:42:25 +0000

    Changed the text on buttons to indicate source

--- app/src/main/java/com/equalexperts/weather1self/response/lib1Self/WeatherEventAttributes.java
+++ app/src/main/java/com/equalexperts/weather1self/response/lib1Self/WeatherEventAttributes.java
@@ -3,7 +3,7 @@ package com.equalexperts.weather1self.response.lib1Self;
 import java.util.Arrays;
 import java.util.List;
 
-public final class WeatherEventAttributes {
+public interface WeatherEventAttributes {
     public static final List<String> OBJECT_TAGS = Arrays.asList("weather", "city");
     public static final List<String> ACTION_TAGS = Arrays.asList("record");
     public static final String PROPERTY = "temperature";
