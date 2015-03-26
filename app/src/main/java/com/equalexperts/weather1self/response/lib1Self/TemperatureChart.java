642ded1bbd79539b817d7f300762c096d879d03c
Gaurav Sakhardande <sakhardande.gaurav@gmail.com>
2015-03-26 00:50:39 +0000

    Fixed compiler error

---
+++ app/src/main/java/com/equalexperts/weather1self/response/lib1Self/TemperatureChart.java
@@ -0,0 +1,10 @@
+package com.equalexperts.weather1self.response.lib1Self;
+
+public class TemperatureChart {
+
+    private String chartUri;
+
+    public String getChartUri() {
+        return chartUri;
+    }
+}
